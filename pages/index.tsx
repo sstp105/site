6ca92dd95dbad5b19ef9b5d720c970d3d5bb93cf
yang052513 <liyang0525.ly@gmail.com>
2021-09-06 12:39:59 -0700

    added scroll animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Page } from 'components/atoms/Layout'
 import { HomeHero } from 'components/templates/Home/HomeHero'
 import { IProfile } from 'types/schema/Profile'
@@ -8,6 +8,8 @@ import { ProfileContext } from 'context/ProfileContext'
 import { homeComponents } from 'components/templates/Home'
 import { API } from 'libs/config/vars'
 import { Spacer } from 'components/atoms/Spacer'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 interface IHomeProps {
   profile: IProfile
@@ -16,6 +18,15 @@ interface IHomeProps {
 }
 
 const Home: React.FC<IHomeProps> = (props) => {
+  useEffect(() => {
+    AOS.init({
+      offset: 300,
+      duration: 1000,
+      easing: 'ease',
+      delay: 100
+    })
+  }, [])
+
   return (
     <ProfileContext.Provider value={props}>
       <HomeHero />
@@ -24,7 +35,11 @@ const Home: React.FC<IHomeProps> = (props) => {
           const { id, component } = homeComponents[key]
           return (
             <React.Fragment key={id}>
-              <section id={id} style={{ position: 'relative' }}>
+              <section
+                id={id}
+                style={{ position: 'relative' }}
+                data-aos="fade-up"
+              >
                 {component}
               </section>
               <Spacer verticalSpace="200px" />
