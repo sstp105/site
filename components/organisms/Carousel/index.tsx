a4f73821b13a1dcc1e7a8ad067e765c4c15a7ba8
yang052513 <liyang0525.ly@gmail.com>
2021-09-08 22:51:06 -0700

    added Carousel component, refactor project detail page

---
+++ components/organisms/Carousel/index.tsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useRef, useState } from 'react'
+import styled from 'styled-components'
+import { Image } from 'components/atoms/Image'
+import { IImage } from 'types/shared/Image'
+
+const Slider = styled.div`
+  margin: 20px 0;
+  height: 550px;
+  position: relative;
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+  }
+  .active {
+    opacity: 1;
+  }
+  .slide {
+    opacity: 0;
+  }
+`
+
+const Previews = styled.div`
+  position: relative;
+  height: 100px;
+  display: flex;
+  overflow-x: scroll;
+  border-radius: 10px;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  &:focus {
+    outline: none;
+  }
+
+  img {
+    width: auto;
+    height: 100%;
+    margin-right: 15px;
+    border-radius: 10px;
+  }
+  .active {
+    opacity: 1;
+  }
+  .slide {
+    opacity: 0.5;
+  }
+`
+
+export interface ICarousel {
+  images: Array<IImage>
+}
+
+export const Carousel: React.FC<ICarousel> = (props) => {
+  const { images } = props
+
+  const [curSlideIndex, setCurSlideIndex] = useState<number>(0)
+  const indexRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!indexRef.current) return
+
+    indexRef.current.focus()
+  }, [])
+
+  const handleArrowKeyNavigation = (keyPressed: KeyboardEvent) => {
+    const { key } = keyPressed
+    if (!indexRef.current) {
+      return
+    }
+
+    if (key === 'ArrowLeft' && curSlideIndex > 0) {
+      setCurSlideIndex((prevIndex) => prevIndex - 1)
+    } else if (key === 'ArrowRight' && curSlideIndex < images.length - 1) {
+      setCurSlideIndex((prevIndex) => prevIndex + 1)
+    }
+  }
+
+  return (
+    <>
+      <Slider>
+        {images.map((elem, index) => (
+          <Image
+            key={elem._id}
+            className={index === curSlideIndex ? 'active' : 'slide'}
+            src={elem.url}
+            alt=""
+            variant="square"
+          />
+        ))}
+      </Slider>
+      <Previews
+        ref={indexRef}
+        onKeyDown={handleArrowKeyNavigation as any}
+        tabIndex={curSlideIndex}
+      >
+        {images.map((elem, index) => (
+          <Image
+            onClick={() => setCurSlideIndex(index)}
+            key={elem._id}
+            className={index === curSlideIndex ? 'active' : 'slide'}
+            src={elem.url}
+            alt=""
+            variant="square"
+          />
+        ))}
+      </Previews>
+    </>
+  )
+}
--- pages/project/[id]/index.tsx
+++ pages/project/[id]/index.tsx
@@ -1,69 +1,15 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import styled, { css } from 'styled-components'
 import { API } from 'libs/config/vars'
 import { IProject, IProjectBase } from 'types/schema/Project'
-import { Image } from 'components/atoms/Image'
 import { Typography } from 'components/atoms/Typography'
-import { Chip } from 'components/molecules/Chip'
-import { Flex } from 'components/atoms/Flex'
 import { Divider } from 'components/atoms/Divider'
 import { Breadcrumb } from 'components/molecules/Breadcrumb'
 import { GetStaticPaths, GetStaticProps } from 'next'
-
-const Container = styled.article`
-  /* max-width: 800px; */
-  margin: 75px auto;
-  max-width: 1140px;
-  padding-left: 20px;
-  padding-right: 20px;
-`
-
-const Slider = styled.div`
-  margin: 20px 0;
-  height: 550px;
-  position: relative;
-  img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-  }
-  .active {
-    opacity: 1;
-  }
-  .slide {
-    opacity: 0;
-  }
-`
-
-const Previews = styled.div`
-  position: relative;
-  height: 100px;
-  display: flex;
-  overflow-x: scroll;
-  border-radius: 10px;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  &:focus {
-    outline: none;
-  }
-
-  img {
-    width: auto;
-    height: 100%;
-    margin-right: 15px;
-    border-radius: 10px;
-  }
-  .active {
-    opacity: 1;
-  }
-  .slide {
-    opacity: 0.5;
-  }
-`
+import { ISeo, Seo } from 'components/templates/Seo'
+import { ChipList } from 'components/organisms/ChipList'
+import { Carousel } from 'components/organisms/Carousel'
+import { Page } from 'components/atoms/Page'
 
 const TextContainer = styled.div`
   display: flex;
@@ -79,77 +25,29 @@ const ProjectDetailPage: React.FC<IProject> = (props) => {
   const { title, category, description, tags, url, images, features, banner } =
     props
   const previews = [banner, ...images]
-  const [curSlideIndex, setCurSlideIndex] = useState<number>(0)
-  const indexRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    if (!indexRef.current) return
-
-    indexRef.current.focus()
-  }, [])
-
-  const handleArrowKeyNavigation = (keyPressed: KeyboardEvent) => {
-    const { key } = keyPressed
-    if (!indexRef.current) {
-      return
-    }
-
-    if (key === 'ArrowLeft' && curSlideIndex > 0) {
-      setCurSlideIndex((prevIndex) => prevIndex - 1)
-    } else if (key === 'ArrowRight' && curSlideIndex < previews.length - 1) {
-      setCurSlideIndex((prevIndex) => prevIndex + 1)
-    }
+  const seoProps: ISeo = {
+    title: title,
+    description: description
   }
-
   return (
-    <div>
-      <Container>
+    <>
+      <Seo {...seoProps} />
+      <Page
+        width={1140}
+        css={css`
+          margin-top: 100px;
+          padding: 0 20px;
+        `}
+      >
         <Breadcrumb />
         <Typography variant="h3">{title}</Typography>
         <Typography variant="caption">{category}</Typography>
         <Typography>{description}</Typography>
+        <ChipList items={tags} variant="outlined" size="small" />
 
-        <Flex>
-          {tags.map((t: string) => (
-            <Chip
-              key={t}
-              label={t}
-              variant="outlined"
-              size="small"
-              css={css`
-                margin-right: 10px;
-                margin-bottom: 10px;
-              `}
-            />
-          ))}
-        </Flex>
-        <Slider>
-          {previews.map((elem, index) => (
-            <Image
-              key={elem._id}
-              className={index === curSlideIndex ? 'active' : 'slide'}
-              src={elem.url}
-              alt=""
-              variant="square"
-            />
-          ))}
-        </Slider>
-        <Previews
-          ref={indexRef}
-          onKeyDown={handleArrowKeyNavigation as any}
-          tabIndex={curSlideIndex}
-        >
-          {previews.map((elem, index) => (
-            <Image
-              onClick={() => setCurSlideIndex(index)}
-              key={elem._id}
-              className={index === curSlideIndex ? 'active' : 'slide'}
-              src={elem.url}
-              alt=""
-              variant="square"
-            />
-          ))}
-        </Previews>
+        <Carousel images={previews} />
+
         <Divider />
         {features.map((elem) => {
           const { title, previews, _id } = elem
@@ -173,8 +71,8 @@ const ProjectDetailPage: React.FC<IProject> = (props) => {
             </TextContainer>
           )
         })}
-      </Container>
-    </div>
+      </Page>
+    </>
   )
 }
 
