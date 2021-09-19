77f4b485fbb3cebf5e2a3ee7a0c9f573bd824576
yang052513 <liyang0525.ly@gmail.com>
2021-09-19 11:29:09 -0700

    resize all video banners

--- components/templates/VideoDetail/index.tsx
+++ components/templates/VideoDetail/index.tsx
@@ -1,37 +1,24 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { Typography } from 'components/atoms/Typography'
 import { ChipList } from 'components/organisms/ChipList'
-import { VideoPlayer } from 'components/organisms/VideoPlayer'
 import styled, { css } from 'styled-components'
-import { IVideoProps } from 'components/molecules/Video'
 import { IVideo } from 'types/schema/Video'
 import { Album } from 'components/organisms/Album'
 import { TITLE } from 'libs/constants/text'
+import { Video } from 'components/molecules/Video'
+import { Panel } from 'components/molecules/Panel'
 
 export const VideoDetail: React.FC<IVideo> = (props) => {
-  const { title, description, publishedDate, tags, banner, url } = props
-
-  const curVideoRef = useRef(null)
-  const [showBanner, setShowBanner] = useState<boolean>(true)
-
-  useEffect(() => {
-    setShowBanner(true)
-  }, [url])
-
-  const play = () => {
-    if (!curVideoRef) return
-
-    setShowBanner(false)
-    curVideoRef.current.controls = true
-    curVideoRef.current.play()
-  }
-
-  const videoProps: IVideoProps = {
-    showBanner: showBanner,
-    onPlay: () => play(),
-    banner: banner,
-    src: url
-  }
+  const {
+    title,
+    description,
+    publishedDate,
+    tags,
+    banner,
+    url,
+    music,
+    thumbsUp
+  } = props
 
   return (
     <>
@@ -45,19 +32,15 @@ export const VideoDetail: React.FC<IVideo> = (props) => {
         {title}
       </Typography>
       <Typography variant="note">{publishedDate}</Typography>
-      <VideoPlayer videoProps={videoProps} ref={curVideoRef} />
+      <Video banner={banner} src={url} />
+      <Panel />
       <Typography variant="body">{description}</Typography>
       <ChipList items={tags} size="small" variant="outlined" />
       <MusicContainer>
         <Typography variant="h6" margin={false}>
           {TITLE.MUSIC}
         </Typography>
-        <Album
-          banner={{
-            url: 'https://lain.bgm.tv/pic/cover/l/14/28/231883_eVji6.jpg'
-          }}
-          size="s"
-        />
+        <Album {...music} size="s" />
       </MusicContainer>
     </>
   )
