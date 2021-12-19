a003b9eb24a7c05a03ef5696d5e4fb4207ddff2d
yang052513 <liyang0525.ly@gmail.com>
2021-12-19 10:24:05 -0800

    re-deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
+// Bundle analyzer plugin
 module.exports = withPlugins([withBundleAnalyzer], {
   inlineImageLimit: false,
   module: {
